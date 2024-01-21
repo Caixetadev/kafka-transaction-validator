5ed2c9c56871a60ecfe6d5fef2bfacf46c472424
Rafael Caixeta <caixetadev@gmail.com>
2024-01-21 15:17:19 +0100

    feat: add consumer

---
+++ transaction/pkg/kafka/consumer.go
@@ -0,0 +1,42 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Consumer struct {
+	reader *kafka.Reader
+}
+
+func NewConsumer(brokers []string, topic string) *Consumer {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   brokers,
+		Topic:     topic,
+		Partition: 0,
+		MaxBytes:  10e6,
+	})
+
+	reader.SetOffset(0)
+
+	return &Consumer{reader: reader}
+}
+
+func (c *Consumer) ReadMessage(ctx context.Context, value interface{}) {
+	m, err := c.reader.ReadMessage(context.Background())
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = json.Unmarshal(m.Value, value)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (c *Consumer) Close() {
+	c.reader.Close()
+}
